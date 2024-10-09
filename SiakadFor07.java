c467cce1740997a57f279050eef19c5b59076e42
Bisminee <fiksilmiah@gmail.com>
2024-10-09 07:33:30 +0700

    Perulangan

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -4,10 +4,16 @@ public class SiakadFor07 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     double nilai, tertinggi = 0, terendah = 100;
+    int lulus=0, tidakLulus=0;
 
         for(int i = 1; i <= 10; i++){
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            if(nilai >= 60){
+                lulus++;
+            }else{
+                tidakLulus++;
+            }
             if(nilai > tertinggi){
                 tertinggi = nilai;
             }
@@ -17,6 +23,9 @@ public class SiakadFor07 {
         }
     System.out.println("Nilai Tertingi: " + tertinggi);
     System.out.println("Nilai Terendah: "+ terendah);
+
+    System.out.println("Siswa Lulus Berjumlah: " + lulus);
+    System.out.println("Siswa Tidak Lulus Berjumlah: " + tidakLulus);
     }
     
 }
